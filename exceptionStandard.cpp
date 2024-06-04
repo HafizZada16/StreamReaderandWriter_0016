43d3d6e456de459f72345b44016c139330c8e7d0
HafizZada16 <hafizzada16@gmail.com>
2024-06-04 18:26:33 +0700

    Add output "Awal Program" and data 7

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -2,3 +2,13 @@
 #include <exception>
 #include <array>
 using namespace std;
+
+int main()
+{
+    cout << "Awal Program" << endl;
+    try {
+        array<int, 3> data = { 6, 7, 8 };
+        cout<<data.at(5)<< endl;
+    }
+    
+}
