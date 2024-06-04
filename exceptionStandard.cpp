e7f945350407a90560a8587e2bf8a80473dd544e
HafizZada16 <hafizzada16@gmail.com>
2024-06-04 18:29:48 +0700

    Add objek exception

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -10,5 +10,8 @@ int main()
         array<int, 3> data = { 6, 7, 8 };
         cout<<data.at(5)<< endl;
     }
-    
+    catch (exception& e) {
+        cout << e.what() << endl;
+        
+    }
 }
