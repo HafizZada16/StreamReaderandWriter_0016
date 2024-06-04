ef5607351fdff773f9d7212d8b29fccf8df46001
HafizZada16 <hafizzada16@gmail.com>
2024-06-04 17:36:50 +0700

    Add if while untuk melakukan perulangan setiap baris

--- iofile.cpp
+++ iofile.cpp
@@ -22,4 +22,12 @@ int main() {
     ifstream infile;
     infile.open("contohfile.txt");
     cout << endl << ">= Membuka dan membaca file " << endl;
+
+    if (infile.is_open())
+    {
+        while (getline(infile, baris))
+        {
+            cout << baris << '\n';
+        }
+    }   
 }
