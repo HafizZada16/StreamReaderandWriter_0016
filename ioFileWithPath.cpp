b3d469e70940f00e07cc209be292383525d533d0
HafizZada16 <hafizzada16@gmail.com>
2024-06-04 17:55:46 +0700

    Add while input text, jika ketik q program berhenti

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -13,5 +13,15 @@ int main() {
     ofstream outfile;
     outfile.open(NamaFile + ".txt", ios::out);
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    while (true) {
+        cout << "- ";
+        getline(cin, baris);
+        if (baris == "q") break;
+        outfile << baris << endl;
+    }
+    outfile.close();
+
     
+    return 0;
 }
